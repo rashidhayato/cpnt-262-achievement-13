32e63348c5ec39751bc7da800896f725f56c0918
rashid <abdulrashid.hayato@edu.sait.ca>
2023-03-10 14:17:25 -0700

    adding new repo

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Using an array method, output the elements of the following array in reverse order:
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+const arr1 = arr.reverse();
+console.log(arr1);
+
+
+// Using a for loop, print all even numbers up to and including n:
+
+const n = 22;
+for (let i = 1; i <= n; i++){
+   if (i % 2 == 0) 
+    console.log(i)
+}
+
+
+// Example output:
+
+// 2 4 6 8 10 12 14 16 18 20 22
